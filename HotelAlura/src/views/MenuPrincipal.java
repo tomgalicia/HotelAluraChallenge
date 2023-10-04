701c1f4fa9af9215a2afee45b43dd25e9bd858bb
tomgalicia <tgalicia@gmail.com>
2023-10-03 19:21:44 -0500

    Primera Version de Hotel Alura

---
+++ HotelAlura/src/views/MenuPrincipal.java
@@ -0,0 +1,182 @@
+package views;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Panel;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Toolkit;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+@SuppressWarnings("serial")
+public class MenuPrincipal extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel labelExit;
+	int xMouse, yMouse;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipal frame = new MenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuPrincipal() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MenuPrincipal.class.getResource("/imagenes/aH-40px.png")));
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 910, 537);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setUndecorated(true);
+
+		
+		Panel panel = new Panel();
+		panel.setBackground(SystemColor.window);
+		panel.setBounds(0, 0, 910, 537);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel imagenFondo = new JLabel("");
+		imagenFondo.setBounds(-50, 0, 742, 501);
+		imagenFondo.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/alura-sl.png")));
+		panel.add(imagenFondo);
+					
+		JLabel Logo = new JLabel("");
+		Logo.setBounds(722, 20, 150, 136);
+		Logo.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/aH-150px.png")));
+		panel.add(Logo);
+		
+		JLabel LetreroSL = new JLabel("Santuario Luciérnagas");
+		LetreroSL.setBounds(712, 100, 180, 200);
+		LetreroSL.setForeground(new Color(33, 97, 140));
+		LetreroSL.setFont(new Font("Roboto", Font.BOLD, 16));
+		panel.add(LetreroSL);
+		
+		JLabel Luciernaga = new JLabel("");
+		Luciernaga.setBounds(772, 180, 150, 156);
+		Luciernaga.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/luciernaga.png")));
+		panel.add(Luciernaga);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 500, 692, 37);
+		panel_1.setBackground(new Color(12, 138, 199));
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblCopyR = new JLabel("Desarrollado por ALURA & Tom Galicia © 2023");
+		lblCopyR.setBounds(215, 11, 284, 19);
+		lblCopyR.setForeground(new Color(240, 248, 255));
+		lblCopyR.setFont(new Font("Roboto", Font.PLAIN, 12));
+		panel_1.add(lblCopyR);
+		
+		JPanel header = new JPanel();
+		header.setBounds(0, 0, 910, 36);
+		header.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				headerMouseDragged(e);
+			     
+			}
+		});
+		header.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				headerMousePressed(e);
+			}
+		});
+		header.setLayout(null);
+		header.setBackground(Color.WHITE);
+		panel.add(header);
+		
+		JPanel btnexit = new JPanel();
+		btnexit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnexit.setBackground(Color.red);
+				labelExit.setForeground(Color.white);
+			}			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				 btnexit.setBackground(Color.white);
+			     labelExit.setForeground(Color.black);
+			}
+		});
+		btnexit.setLayout(null);
+		btnexit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+		btnexit.setBackground(Color.WHITE);
+		btnexit.setBounds(857, 0, 53, 36);
+		header.add(btnexit);
+		
+		labelExit = new JLabel("X");
+		labelExit.setBounds(0, 0, 53, 36);
+		btnexit.add(labelExit);
+		labelExit.setHorizontalAlignment(SwingConstants.CENTER);
+		labelExit.setFont(new Font("Roboto", Font.PLAIN, 18));
+		
+		JPanel btnLogin = new JPanel();
+		btnLogin.setBounds(724, 300, 139, 132);
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Login login = new Login();
+				login.setVisible(true);
+				dispose();
+			}
+		});
+		btnLogin.setLayout(null);
+		btnLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+		btnLogin.setBackground(SystemColor.window);
+		panel.add(btnLogin);
+		
+		JLabel lblLogin = new JLabel("");
+		lblLogin.setBounds(0, 0, 156,168); 
+		btnLogin.add(lblLogin);
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setIcon(new ImageIcon(MenuPrincipal.class.getResource("/imagenes/login-button.png")));
+		
+	}
+	
+	private void headerMousePressed(java.awt.event.MouseEvent evt) {
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_headerMousePressed
+
+    private void headerMouseDragged(java.awt.event.MouseEvent evt) {
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xMouse, y - yMouse);
+}
+}
